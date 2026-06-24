d4babd4c02305121bdb82420c277bf6fbc75afed
DanielCScience <daniel.0trabalho@gmail.com>
2026-06-24 20:04:24 -0300

    Update ItemPedido.java

--- src/main/java/com/api/vendas/model/ItemPedido.java
+++ src/main/java/com/api/vendas/model/ItemPedido.java
@@ -1,5 +1,6 @@
 package com.api.vendas.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class ItemPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pedido_id", nullable = false)
     private Pedido pedido;
